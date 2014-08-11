590b702a2f434f787dc83b9a351d5663e54e2578
William Douglas <william.douglas@intel.com>
2014-08-11 12:23:21 -0700

    Cleanup ister_test to pass pylint and pep8
    Add one exception for Exception too broad error from pylint as we do
    not care about subclassing Exception at this point as it is used to
    abort progress with an error message on any error. If the design
    changes this exception should be revisited.

--- ister_test.py
+++ ister_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Linux installation template system test suite"""
 
 #
 # This file is part of ister.
@@ -17,69 +18,118 @@
 # Boston, MA 02110-1301 USA
 #
 
+# If we see an exception it is always fatal so the broad exception
+# warning isn't helpful.
+# pylint: disable=W0703
+
 import ister
 import json
-import os
 import time
 
+
 def good_min_template():
-    return u'{"ImageSourceType": "local", "ImageSourceLocation": "file:///good.raw.xz"}'
+    """Return string representation of good_min_template"""
+    return u'{"ImageSourceType": "local", "ImageSourceLocation": \
+    "file:///good.raw.xz"}'
+
+
 def good_min_remote_template():
-    return u'{"ImageSourceType": "local", "ImageSourceLocation": "http://10.0.2.2:8001/good.raw.xz"}'
+    """Return string representation of good_min_remote_template"""
+    return u'{"ImageSourceType": "local", "ImageSourceLocation": \
+    "http://10.0.2.2:8001/good.raw.xz"}'
+
+
 def good_user_template():
-    return u'{"ImageSourceType": "local", "ImageSourceLocation": "file:///good.raw.xz", \
+    """Return string representation of good_user_template"""
+    return u'{"ImageSourceType": "local", "ImageSourceLocation": \
+    "file:///good.raw.xz", \
     "Users": [{"username": "test"}]}'
+
+
 def good_user_key_template():
-    return u'{"ImageSourceType": "local", "ImageSourceLocation": "file:///good.raw.xz", \
+    """Return string representation of good_user_key_template"""
+    return u'{"ImageSourceType": "local", "ImageSourceLocation": \
+    "file:///good.raw.xz", \
     "Users": [{"username": "test", "key": "file:///root/key.pub"}]}'
+
+
 def good_user_uid_template():
-    return u'{"ImageSourceType": "local", "ImageSourceLocation": "file:///good.raw.xz", \
+    """Return string representation of good_user_uid_template"""
+    return u'{"ImageSourceType": "local", "ImageSourceLocation": \
+    "file:///good.raw.xz", \
     "Users": [{"username": "test", "uid": 1000}]}'
+
+
 def good_user_sudop_template():
-    return u'{"ImageSourceType": "local", "ImageSourceLocation": "file:///good.raw.xz", \
+    """Return string representation of good_user_sudop_template"""
+    return u'{"ImageSourceType": "local", "ImageSourceLocation": \
+    "file:///good.raw.xz", \
     "Users": [{"username": "test", "sudo": "password"}]}'
+
+
 def good_disk_template():
-    return u'{"ImageSourceType": "local", "ImageSourceLocation": "file:///good.raw.xz", \
+    """Return string representation of good_disk_template"""
+    return u'{"ImageSourceType": "local", "ImageSourceLocation": \
+    "file:///good.raw.xz", \
     "PartitionLayout": \
-    [{"disk": "sdb", "partition": 1, "size": "512M", "type": "EFI"}, {"disk": "sdb", "partition": 2, \
-    "size": "512M", "type": "swap"}, {"disk": "sdb", "partition": 3, "size": "rest", "type": "linux"}], \
+    [{"disk": "sdb", "partition": 1, "size": "512M", "type": "EFI"}, \
+    {"disk": "sdb", "partition": 2, \
+    "size": "512M", "type": "swap"}, {"disk": "sdb", "partition": 3, \
+    "size": "rest", "type": "linux"}], \
     "FilesystemTypes": \
-    [{"disk": "sdb", "partition": 1, "type": "vfat"}, {"disk": "sdb", "partition": 2, "type": "swap"}, \
+    [{"disk": "sdb", "partition": 1, "type": "vfat"}, {"disk": "sdb", \
+    "partition": 2, "type": "swap"}, \
     {"disk": "sdb", "partition": 3, "type": "ext4"}], \
     "PartitionMountPoints": \
-    [{"disk": "sdb", "partition": 1, "mount": "/boot"}, {"disk": "sdb", "partition": 3, "mount": "/"}]}'
+    [{"disk": "sdb", "partition": 1, "mount": "/boot"}, {"disk": "sdb", \
+    "partition": 3, "mount": "/"}]}'
+
+
 def full_user_install_template():
-    return u'{"ImageSourceType": "local", "ImageSourceLocation": "file:///good.raw.xz", \
-    "PartitionLayout": \
-    [{"disk": "sdb", "partition": 1, "size": "512M", "type": "EFI"}, {"disk": "sdb", "partition": 2, \
-    "size": "512M", "type": "swap"}, {"disk": "sdb", "partition": 3, "size": "rest", "type": "linux"}], \
+    """Return string representation of full_user_install_template"""
+    return u'{"ImageSourceType": "local", "ImageSourceLocation": \
+    "file:///good.raw.xz", "PartitionLayout": \
+    [{"disk": "sdb", "partition": 1, "size": "512M", "type": "EFI"}, \
+    {"disk": "sdb", "partition": 2, "size": "512M", "type": "swap"}, \
+    {"disk": "sdb", "partition": 3, "size": "rest", "type": "linux"}], \
     "FilesystemTypes": \
-    [{"disk": "sdb", "partition": 1, "type": "vfat"}, {"disk": "sdb", "partition": 2, "type": "swap"}, \
+    [{"disk": "sdb", "partition": 1, "type": "vfat"}, {"disk": "sdb", \
+    "partition": 2, "type": "swap"}, \
     {"disk": "sdb", "partition": 3, "type": "ext4"}], \
     "PartitionMountPoints": \
-    [{"disk": "sdb", "partition": 1, "mount": "/boot"}, {"disk": "sdb", "partition": 3, "mount": "/"}], \
-    "Users": [{"username": "user", "key": "file:///root/key.pub", "uid": 1001, "sudo": "password"}]}'
+    [{"disk": "sdb", "partition": 1, "mount": "/boot"}, {"disk": "sdb", \
+    "partition": 3, "mount": "/"}], \
+    "Users": [{"username": "user", "key": "file:///root/key.pub", \
+    "uid": 1001, "sudo": "password"}]}'
+
 
 def read_good_local_conf():
+    """Run read_good_local_conf test"""
     template_file = ister.get_template_location("/root/good-ister.conf")
     if template_file != u"file:///tmp/template.json":
         raise Exception("Incorrect template file path")
 
+
 def load_min_good_local_template():
+    """Run load_min_good_local_template test"""
     filename = "file:///root/min-good.json"
     template = ister.get_template(filename)
     good = json.loads(good_min_template())
     if template != good:
         raise Exception("JSON template doesn't match")
 
+
 def load_min_good_remote_template():
+    """Run load_min_good_remote_template test"""
     filename = "http://10.0.2.2:8001/min-good.json"
     template = ister.get_template(filename)
     good = json.loads(good_min_template())
     if template != good:
         raise Exception("JSON remote template doesn't match")
 
+
 def get_valid_remote_image():
+    """Run get_valid_remote_image test"""
     template = json.loads(good_min_remote_template())
     try:
         ister.get_source_image(template)
@@ -88,123 +138,151 @@ def get_valid_remote_image():
     if template["ImageSourceLocation"] != "file:///image.xz":
         raise Exception("Failed to update ImageSourceLocation")
 
+
 def validate_good_template():
-    good_templates = [good_min_template, good_min_remote_template, good_user_template, good_user_key_template, good_user_uid_template, good_user_sudop_template, good_disk_template, full_user_install_template]
+    """Run validate_good_template test"""
+    good_templates = [good_min_template, good_min_remote_template,
+                      good_user_template, good_user_key_template,
+                      good_user_uid_template, good_user_sudop_template,
+                      good_disk_template, full_user_install_template]
 
     for template_string in good_templates:
         template = json.loads(template_string())
         try:
             ister.validate_template(template)
-        except Exception as e:
-            raise Exception("JSON template {0} ({1}) is invalid: {2}".format(template_string.__name__, template_string(), e))
+        except Exception as exep:
+            raise Exception("JSON template {0} ({1}) is invalid: {2}"
+                            .format(template_string.__name__,
+                                    template_string(), exep))
+
 
 def validate_fs_default_detection():
+    """Run validate_fs_default_detection test"""
     template = json.loads(good_min_template())
     try:
         dev = ister.find_target_disk()
-    except Exception as e:
-        raise Exception("Unable to find target device: ", e)
+    except Exception as exep:
+        raise Exception("Unable to find target device: ", exep)
     try:
         ister.insert_fs_defaults(template)
-    except Exception as e:
-        raise Exception("Unable to add default disk target: ", e)
+    except Exception as exep:
+        raise Exception("Unable to add default disk target: ", exep)
 
     if not template.get("PartitionLayout"):
         raise Exception("Failed to insert PartitionLayout to template")
-    if template["PartitionLayout"] != [{"disk" : dev, "partition" : 1, "size" : "512M", "type" : "EFI" },
-                                       {"disk" : dev, "partition" : 2, "size" : "rest", "type" : "linux" }]:
+    if template["PartitionLayout"] != [{"disk": dev, "partition": 1,
+                                        "size": "512M", "type": "EFI"},
+                                       {"disk": dev, "partition": 2,
+                                        "size": "rest", "type": "linux"}]:
         raise Exception("Failed to setup PartitionLayout correctly")
     if not template.get("FilesystemTypes"):
         raise Exception("Failed to insert FilesystemTypes to template")
-    if template["FilesystemTypes"] != [{"disk" : dev, "partition" : 1, "type" : "vfat" },
-                                      {"disk" : dev, "partition" : 2, "type" : "ext4" }]:
+    if template["FilesystemTypes"] != [{"disk": dev, "partition": 1,
+                                        "type": "vfat"},
+                                       {"disk": dev, "partition": 2,
+                                        "type": "ext4"}]:
         raise Exception("Failed to setup FilesystemTypes correctly")
     if not template.get("PartitionMountPoints"):
         raise Exception("Failed to insert PartitionMountPoints to template")
-    if template["PartitionMountPoints"] != [{"disk" : dev, "partition" : 1, "mount" : "/boot" },
-                                            {"disk" : dev, "partition" : 2, "mount" : "/" }]:
+    if template["PartitionMountPoints"] != [{"disk": dev, "partition": 1,
+                                             "mount": "/boot"},
+                                            {"disk": dev, "partition": 2,
+                                             "mount": "/"}]:
         raise Exception("Failed to setup PartitionMountPoints correctly")
 
+
 def validate_full_user_install():
+    """Run validate_full_user_install test"""
     template = json.loads(full_user_install_template())
     try:
         ister.create_partitions(template)
-    except Exception as e:
-        raise Exception("Unable to create partitions ({0}): {1}".format(template["PartitionLayout"], e))
+    except Exception as exep:
+        raise Exception("Unable to create partitions ({0}): {1}"
+                        .format(template["PartitionLayout"], exep))
 
     try:
         ister.create_filesystems(template)
-    except Exception as e:
-        raise Exception("Unable to create filesystems ({0}): {1}".format(template["FilesystemTypes"], e))
+    except Exception as exep:
+        raise Exception("Unable to create filesystems ({0}): {1}"
+                        .format(template["FilesystemTypes"], exep))
 
     try:
-        (s, t) = ister.setup_mounts(template)
-    except Exception as e:
-        raise Exception("Unable to setup mount points ({0}): {1}".format(template["PartitionMountPoints"], e))
+        (source, target) = ister.setup_mounts(template)
+    except Exception as exep:
+        raise Exception("Unable to setup mount points ({0}): {1}"
+                        .format(template["PartitionMountPoints"], exep))
 
     try:
-        ister.copy_files(s, t)
-    except Exception as e:
-        raise Exception("Unable to install OS: {0}".format(e))
+        ister.copy_files(source, target)
+    except Exception as exep:
+        raise Exception("Unable to install OS: {0}".format(exep))
 
     try:
         uuids = ister.get_uuids(template)
-    except Exception as e:
-        raise Exception("Unable to get uuids in ({0}): {1}".format(template, e))
+    except Exception as exep:
+        raise Exception("Unable to get uuids in ({0}): {1}"
+                        .format(template, exep))
 
     try:
-        ister.update_loader(uuids, t)
-    except Exception as e:
-        raise Exception("Unable to update loader conf: {0}".format(e))
+        ister.update_loader(uuids, target)
+    except Exception as exep:
+        raise Exception("Unable to update loader conf: {0}".format(exep))
 
     try:
-        ister.update_fstab(uuids, t)
-    except Exception as e:
-        raise Exception("Unable to update fstab: {0}".format(e))
+        ister.update_fstab(uuids, target)
+    except Exception as exep:
+        raise Exception("Unable to update fstab: {0}".format(exep))
 
     try:
-        ister.add_users(template, t)
-    except Exception as e:
-        raise Exception("Unable to add users: {0}".format(e))
+        ister.add_users(template, target)
+    except Exception as exep:
+        raise Exception("Unable to add users: {0}".format(exep))
 
     try:
-        ister.cleanup(s, t)
-    except Exception as e:
-        raise Exception("Unable to cleanup after install: {}".format(e))
+        ister.cleanup(source, target)
+    except Exception as exep:
+        raise Exception("Unable to cleanup after install: {}".format(exep))
+
 
 def validate_remote_image_setup():
+    """Run validate_remote_image_setup test"""
     template = json.loads(good_min_remote_template())
     ister.get_source_image(template)
     ister.insert_fs_defaults(template)
     try:
         ister.create_partitions(template)
-    except Exception as e:
-        raise Exception("Unable to create partitions ({0}): {1}".format(template["PartitionLayout"], e))
+    except Exception as exep:
+        raise Exception("Unable to create partitions ({0}): {1}"
+                        .format(template["PartitionLayout"], exep))
 
     try:
         ister.create_filesystems(template)
-    except Exception as e:
-        raise Exception("Unable to create filesystems ({0}): {1}".format(template["FilesystemTypes"], e))
+    except Exception as exep:
+        raise Exception("Unable to create filesystems ({0}): {1}"
+                        .format(template["FilesystemTypes"], exep))
 
     try:
-        (s, t) = ister.setup_mounts(template)
-    except Exception as e:
-        raise Exception("Unable to setup mount points ({0}): {1}".format(template["PartitionMountPoints"], e))
+        (source, target) = ister.setup_mounts(template)
+    except Exception as exep:
+        raise Exception("Unable to setup mount points ({0}): {1}"
+                        .format(template["PartitionMountPoints"], exep))
 
     try:
-        ister.cleanup(s, t)
-    except Exception as e:
-        raise Exception("Unable to cleanup after install: {}".format(e))
+        ister.cleanup(source, target)
+    except Exception as exep:
+        raise Exception("Unable to cleanup after install: {}".format(exep))
+
 
 def run_tests(tests):
+    """Run ister test suite"""
     flog = open("/root/test-log", "w")
 
     for test in tests:
         try:
             test()
-        except Exception as e:
-            print("Test: {0} failed: {1}.".format(test.__name__, e))
-            flog.write("Test: {0} failed: {1}.\n".format(test.__name__, e))
+        except Exception as exep:
+            print("Test: {0} failed: {1}.".format(test.__name__, exep))
+            flog.write("Test: {0} failed: {1}.\n".format(test.__name__, exep))
         else:
             print("Test: {0} passed.".format(test.__name__))
             flog.write("Test: {0} passed.\n".format(test.__name__))
@@ -214,7 +292,7 @@ def run_tests(tests):
 if __name__ == '__main__':
     # In case network-online.service doesn't work right
     time.sleep(3)
-    tests = [
+    TESTS = [
         read_good_local_conf,
         load_min_good_local_template,
         load_min_good_remote_template,
@@ -225,4 +303,4 @@ if __name__ == '__main__':
         validate_remote_image_setup
     ]
 
-    run_tests(tests)
+    run_tests(TESTS)
